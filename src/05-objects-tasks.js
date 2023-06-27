297a0fa904912507c5dadfbc983d1a447c772be3
johngaalt <rajon777666@gmail.com>
2023-06-27 13:42:51 +0300

    feat: fromJson

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -55,8 +55,15 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const data = JSON.parse(json);
+  const obj = Object.create(proto);
+
+  Object.keys(data).forEach((key) => {
+    obj[key] = data[key];
+  });
+
+  return obj;
 }
 
 /**
